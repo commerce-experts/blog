832e7fde86a57e715a1ed13895fdb7b2ca27773a
commerce-experts <anwaatwork@gmail.com>
2016-03-15 08:59:28 +0100

    updated config

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ $(function() {
 	CMS.init({
 
 		// Name of your site or location of logo file ,relative to root directory (img/logo.png)
-		siteName: '',
+		siteName: 'Blog | CXP',
 
 		// Tagline for your site
 		siteTagline: '/ blogging about data driven eBusiness',
